d40adb876099b44d872faa557dc937a025542524
Multfinite <multfinite@gmail.com>
2024-10-07 22:54:17 +0300

    Formatting definitions (serialize/deserialize/parse)

---
+++ src/Core/core.formatting.impl.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include "core.hpp"
+
+#include <Utilities/TemplateDef.h>
+
+#include <optional>
+
+/*
+* Place here all specializations of
+* - parse(...)
+* - serialize(...)
+* - deserialize(...)
+*
+* If will nedeed you can create core.formatting.impl.cpp
+*/
+
+#define PARSE_VALUE_DEFAULT(T) \
+template<bool Allocate = true> \
+void parse<T, Allocate>(T& data, CCINIClass& ini, INI_EX& parser, std::string const& section, std::string const& prefix) \
+{ \
+	detail::read<T, Allocate>(data, parser, section.c_str(), prefix.c_str()); \
+} \
+template<bool Allocate = true> \
+void parse<std::optional<T>, Allocate>(std::optional<T>& data, CCINIClass& ini, INI_EX& parser, std::string const& section, std::string const& prefix) \
+{ \
+	if (parser.ReadString(section.c_str(), prefix.c_str())) \
+	{ \
+		const char* val = parser.value(); \
+		\
+		if (!_strcmpi(val, "<default>") || INIClass::IsBlank(val)) \
+		{ \
+			data.reset(); \
+			return; \
+		} \
+		detail::read<T, Allocate>(*data, parser, pSection, pKey); \
+		data = *data; \
+	} \
+} \
+
+namespace Formatter
+{
+	PARSE_VALUE_DEFAULT(uint8_t);
+	PARSE_VALUE_DEFAULT(uint16_t);
+	PARSE_VALUE_DEFAULT(uint32_t);
+	PARSE_VALUE_DEFAULT(uint64_t);
+
+	PARSE_VALUE_DEFAULT(int8_t);
+	PARSE_VALUE_DEFAULT(int16_t);
+	PARSE_VALUE_DEFAULT(int32_t);
+	PARSE_VALUE_DEFAULT(int64_t);
+
+	PARSE_VALUE_DEFAULT(bool);
+	PARSE_VALUE_DEFAULT(float);
+	PARSE_VALUE_DEFAULT(double);
+
+	PARSE_VALUE_DEFAULT(ArmorType);
+
+	template<typename T>
+	T const& serialize(T const& data, std::ostream& stream);
+
+	template<typename T>
+	T& deserialize(T& data, std::istream& stream);
+}
+
+#undef PARSE_INT_DEFAULT
