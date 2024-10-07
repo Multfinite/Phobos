8a416d9f45901b420afb2f0d978f43538dfa63bc
Multfinite <multfinite@gmail.com>
2024-10-07 22:56:10 +0300

    dirty formatting

--- src/Core/extension.bullet_type.hpp
+++ src/Core/extension.bullet_type.hpp
@@ -4,13 +4,48 @@
 
 #include "extension.object_type.hpp"
 
+#include <Helpers/Macro.h>
+#include <Utilities/TemplateDef.h>
+#include <New/Type/LaserTrailTypeClass.h>
+#include <Ext/Bullet/Trajectories/PhobosTrajectory.h>
+
+#include <optional>
+
 template<> class Extension<BulletTypeClass> : public Extension<ObjectTypeClass>
 {
 	__EXTENSION_BODY(BulletTypeClass, Extension<ObjectTypeClass>::Offset)
 public:
 	using parent_type = Extension<ObjectTypeClass>;
-protected:
+public:
 	constexpr Extension(base_type& base) : parent_type(base) { }
+
+	ArmorType* Armor;
+	bool Interceptable;
+	bool Interceptable_DeleteOnIntercept;
+	WeaponTypeClass* Interceptable_WeaponOverride;
+	ValueableIdxVector<LaserTrailTypeClass> LaserTrail_Types;
+	double Gravity;
+
+	PhobosTrajectoryType* TrajectoryType;// TODO: why not unique_ptr
+	double Trajectory_Speed;
+
+	bool Shrapnel_AffectsGround;
+	bool Shrapnel_AffectsBuildings;
+	std::optional<bool> SubjectToLand;
+	bool SubjectToLand_Detonate;
+	std::optional<bool> SubjectToWater;
+	bool SubjectToWater_Detonate;
+
+	std::optional<Leptons> ClusterScatter_Min;
+	std::optional<Leptons> ClusterScatter_Max;
+
+	bool AAOnly;
+	bool Arcing_AllowElevationInaccuracy;
+	WeaponTypeClass* ReturnWeapon;
+
+	// Ares 0.7
+	std::optional<Leptons> BallisticScatter_Min;
+	std::optional<Leptons> BallisticScatter_Max;
 };
 
 template<> struct Proxy<BulletTypeClass> : public ObjectTypeClass
@@ -19,3 +54,26 @@ template<> struct Proxy<BulletTypeClass> : public ObjectTypeClass
 	__PROXY_MEMORY_MANAGEMENT
 };
 
+template<> struct Formatter::Metadata<BulletTypeClass>
+{
+	using base_type = BulletTypeClass;
+	using extension_type = Extension<BulletTypeClass>;
+
+	static constexpr auto chain() noexcept
+	{
+		auto v = &extension_type::Armor;
+		using t = decltype(((extension_type*) nullptr)->*v);
+
+		auto const chain = Formatter::make_chain(static_cast<extension_type*>(nullptr)
+			, &extension_type::Armor, "Armor"
+			, &extension_type::Interceptable, "Interceptable"
+			, &extension_type::Interceptable_DeleteOnIntercept, "Interceptable.DeleteOnIntercept"
+			, &extension_type::Interceptable_WeaponOverride, "Interceptable.WeaponOverride"
+			, &extension_type::Gravity, "Gravity"
+			, &extension_type::TrajectoryType, "Trajectory"
+			, &extension_type::Trajectory_Speed, "Trajectory.Speed"
+			, &extension_type::Shrapnel_AffectsGround
+		);
+		return chain;
+	}
+};
--- src/Core/extension.techno.hpp
+++ src/Core/extension.techno.hpp
@@ -4,13 +4,64 @@
 
 #include "extension.radio.hpp"
 
+#include <InfantryClass.h>
+#include <AnimClass.h>
+
+#include <Helpers/Macro.h>
+#include <Utilities/Container.h>
+#include <Utilities/TemplateDef.h>
+#include <Utilities/Macro.h>
+#include <New/Entity/ShieldClass.h>
+#include <New/Entity/LaserTrailClass.h>
+
+class BulletClass;
+
 template<> class Extension<TechnoClass> : public Extension<RadioClass>
 {
 	__EXTENSION_BODY(TechnoClass, Extension<RadioClass>::Offset)
 public:
 	using parent_type = Extension<RadioClass>;
 protected:
-	constexpr Extension(base_type& base) : parent_type(base) { }
+	constexpr Extension(base_type& base) : parent_type(base)
+		, TypeExtData { nullptr }
+		, Shield {}
+		, LaserTrails {}
+		, ReceiveDamage { false }
+		, LastKillWasTeamTarget { false }
+		, PassengerDeletionTimer {}
+		, CurrentShieldType { nullptr }
+		, LastWarpDistance {}
+		, AutoDeathTimer {}
+		, MindControlRingAnimType { nullptr }
+		, DamageNumberOffset { INT32_MIN }
+		, OriginalPassengerOwner {}
+		, IsInTunnel { false }
+		, HasBeenPlacedOnMap { false }
+		, DeployFireTimer {}
+		, ForceFullRearmDelay { false }
+		, WHAnimRemainingCreationInterval { 0 }
+	{ }
+public:
+	Extension<TechnoTypeClass>* TypeExtData;
+	std::unique_ptr<ShieldClass> Shield;
+	std::vector<LaserTrailClass> LaserTrails;
+	bool ReceiveDamage;
+	bool LastKillWasTeamTarget;
+	CDTimerClass PassengerDeletionTimer;
+	ShieldTypeClass* CurrentShieldType;
+	int LastWarpDistance;
+	CDTimerClass AutoDeathTimer;
+	AnimTypeClass* MindControlRingAnimType;
+	int DamageNumberOffset;
+	bool IsInTunnel;
+	bool HasBeenPlacedOnMap; // Set to true on first Unlimbo() call.
+	CDTimerClass DeployFireTimer;
+	bool ForceFullRearmDelay;
+	int WHAnimRemainingCreationInterval;
+
+	// Used for Passengers.SyncOwner.RevertOnExit instead of TechnoClass::InitialOwner / OriginallyOwnedByHouse,
+	// as neither is guaranteed to point to the house the TechnoClass had prior to entering transport and cannot be safely overridden.
+	HouseClass* OriginalPassengerOwner;
 };
 
 template<> struct Proxy<TechnoClass> : public RadioClass
